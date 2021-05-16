a3afcb039b15263af80d91ba66f39847fcb204ef
Evan Siegel <siegel.evan@gmail.com>
2021-05-16 15:56:08 -0700

    Adds Color.inverse

--- Onitama/Color.ts
+++ Onitama/Color.ts
@@ -1,3 +1,12 @@
 export type Color = "blue" | "red";
 
+export const inverse = (color: Color): Color => {
+  switch (color) {
+    case "blue":
+      return "red";
+    case "red":
+      return "blue";
+  }
+};
+
 export default Color;
